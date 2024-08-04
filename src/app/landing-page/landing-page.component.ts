e6c33190c828db1ec7b9ca5fca4f671d043708e0
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-08-03 19:04:43 -0500

    fetch resources

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,25 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ResourceService } from './service/resource.service';
+import { Resource } from './model/resource';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
 })
-export class LandingPageComponent {
-  onRegister() {
-    console.log('Register button clicked');
-  }
-
+export class LandingPageComponent implements OnInit {
+  foodResources: Resource[] = [];
   showOxalateComponent = false;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private resourceService: ResourceService
+  ) {
     this.route.queryParams.subscribe((params) => {
       if (params['showOxalate']) {
         this.showOxalateComponent = true;
         this.scrollToOxalate();
       }
     });
+  }
+
+  ngOnInit(): void {
+    this.foodResources = this.resourceService.getFoodResources();
+  }
+
+  onRegister() {
+    console.log('Register button clicked');
   }
 
   scrollToOxalate() {
