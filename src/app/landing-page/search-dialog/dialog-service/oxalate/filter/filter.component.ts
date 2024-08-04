2031da1117a828323b83410b4b89dc5184f4c76e
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-08-04 18:59:17 -0500

    remove resource link

--- src/app/landing-page/search-dialog/dialog-service/oxalate/filter/filter.component.ts
+++ src/app/landing-page/search-dialog/dialog-service/oxalate/filter/filter.component.ts
@@ -1,27 +1,3 @@
-// import { Component, EventEmitter, Output } from '@angular/core';
-// import { Filter } from './model/filter';
-
-// @Component({
-//   selector: 'app-filter',
-//   templateUrl: './filter.component.html',
-//   styleUrls: ['./filter.component.css'],
-// })
-// export class FilterComponent {
-//   filters: Filter = {};
-
-//   @Output() filterChanged = new EventEmitter<Filter>();
-
-//   applyFilters(): void {
-//     console.log('Applying filters On Filter COmponent:', this.filters); // Log filters being applied
-//     this.filterChanged.emit(this.filters);
-//   }
-
-//   clearFilters(): void {
-//     this.filters = {}; // Reset filters
-//     console.log('Clearing filters'); // Log when filters are cleared
-//     this.filterChanged.emit(this.filters); // Emit empty filters
-//   }
-// }
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Filter } from './model/filter';
 
