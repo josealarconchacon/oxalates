dded42e646e7a1f70576568b54e7cf06f85daf39
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-09-03 17:56:28 -0400

    responsive nav

--- src/app/landing-page/header/header.component.ts
+++ src/app/landing-page/header/header.component.ts
@@ -1,4 +1,44 @@
-import { Component } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-header',
+//   templateUrl: './header.component.html',
+//   styleUrls: ['./header.component.css'],
+// })
+// export class HeaderComponent implements OnInit {
+//   constructor(private router: Router) {}
+
+//   ngOnInit(): void {}
+
+//   openNav() {
+//     const mySidenav = document.getElementById(
+//       'mySidenav'
+//     ) as HTMLElement | null;
+//     if (mySidenav) {
+//       mySidenav.style.width = '250px';
+//     }
+//   }
+
+//   closeNav() {
+//     const mySidenav = document.getElementById(
+//       'mySidenav'
+//     ) as HTMLElement | null;
+//     if (mySidenav) {
+//       mySidenav.style.width = '0';
+//     }
+//   }
+
+//   search() {
+//     this.router.navigate(['/oxalate']);
+//   }
+
+//   goToLandingPage() {
+//     this.router.navigate(['/']);
+//   }
+// }
+
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,25 +46,15 @@ import { Router } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isNavOpen = false; // Tracks whether the nav is open
+
   constructor(private router: Router) {}
 
-  openNav() {
-    const mySidenav = document.getElementById(
-      'mySidenav'
-    ) as HTMLElement | null;
-    if (mySidenav) {
-      mySidenav.style.width = '250px';
-    }
-  }
+  ngOnInit(): void {}
 
-  closeNav() {
-    const mySidenav = document.getElementById(
-      'mySidenav'
-    ) as HTMLElement | null;
-    if (mySidenav) {
-      mySidenav.style.width = '0';
-    }
+  toggleNav() {
+    this.isNavOpen = !this.isNavOpen; // Toggle the navigation state
   }
 
   search() {
