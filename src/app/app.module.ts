e2ac02ccbf52d34bb9d945bb619551273fff8c30
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-09-10 16:48:15 -0400

    fb config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -9,6 +9,10 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { environment } from 'src/environments/environment.development';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +27,23 @@ import { ResourcesComponent } from './landing-page/resources/resources.component
 import { BenefitsComponent } from './landing-page/benefits/benefits.component';
 import { ManagingOxalateComponent } from './landing-page/managing-oxalate/managing-oxalate.component';
 import { ViewMoreComponent } from './landing-page/dialog-service/oxalate/view-more/view-more.component';
+import { AlertComponent } from './landing-page/dialog-service/oxalate/shared/alert/alert.component';
+import { ConfigService } from 'src/assets/config/config.service';
+
+export function initializeApp(configService: ConfigService) {
+  return (): Promise<void> => {
+    return new Promise<void>((resolve, reject) => {
+      configService.getConfig().subscribe(
+        (config) => {
+          // Set the Firebase configuration dynamically
+          environment.firebaseConfig = config;
+          resolve();
+        },
+        (error) => reject(error)
+      );
+    });
+  };
+}
 
 @NgModule({
   declarations: [
@@ -36,6 +57,7 @@ import { ViewMoreComponent } from './landing-page/dialog-service/oxalate/view-mo
     BenefitsComponent,
     ManagingOxalateComponent,
     ViewMoreComponent,
+    AlertComponent,
   ],
   imports: [
     FormsModule,
@@ -53,8 +75,20 @@ import { ViewMoreComponent } from './landing-page/dialog-service/oxalate/view-mo
     FormsModule,
     ReactiveFormsModule,
     ScrollingModule,
+    MatIconModule,
+    MatExpansionModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAnalyticsModule,
   ],
-  providers: [],
+  providers: [
+    ConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      deps: [ConfigService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
