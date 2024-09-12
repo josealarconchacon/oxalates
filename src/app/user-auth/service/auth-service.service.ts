a28560317aca2a20e17fffa0d2de41e70f81c097
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-09-11 20:23:07 -0400

    auth service method

--- src/app/user-auth/service/auth-service.service.ts
+++ src/app/user-auth/service/auth-service.service.ts
@@ -1,30 +1,75 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import firebase from 'firebase/compat/app';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private isAuthenticated = false; // Replace this with actual authentication logic
-
-  constructor(private router: Router) {}
-
-  checkAuth(): boolean {
-    return this.isAuthenticated;
+  constructor(private afAuth: AngularFireAuth, private router: Router) {
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        localStorage.setItem('user', JSON.stringify(user));
+      } else {
+        localStorage.removeItem('user');
+      }
+    });
   }
 
-  // Method to navigate to sign-in page
-  redirectToSignIn(): void {
-    this.router.navigate(['/auth']);
+  async signUp(email: string, password: string): Promise<void> {
+    try {
+      await this.afAuth.createUserWithEmailAndPassword(email, password);
+      this.router.navigate(['/profile']);
+    } catch (error) {
+      console.error('Error during sign up:', error);
+    }
   }
 
-  // Method to simulate user sign-in
-  signIn(): void {
-    this.isAuthenticated = true; // Set authentication status
+  async signIn(email: string, password: string): Promise<void> {
+    try {
+      await this.afAuth.signInWithEmailAndPassword(email, password);
+      this.router.navigate(['/profile']);
+    } catch (error) {
+      console.error('Error during sign in:', error);
+    }
   }
 
-  signOut(): void {
-    this.isAuthenticated = false; // Clear authentication status
-    this.router.navigate(['/']); // Navigate to home or another appropriate page
+  async signOut(): Promise<void> {
+    try {
+      await this.afAuth.signOut();
+      localStorage.removeItem('user');
+      this.router.navigate(['/auth']);
+    } catch (error) {
+      console.error('Error during sign out:', error);
+    }
+  }
+
+  getCurrentUser() {
+    return this.afAuth.authState;
+  }
+
+  async googleSignIn(): Promise<void> {
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      await this.afAuth.signInWithPopup(provider);
+      this.router.navigate(['/oxalate']);
+    } catch (error) {
+      console.error('Error during Google sign-in:', error);
+    }
+  }
+
+  async facebookSignIn(): Promise<void> {
+    try {
+      const provider = new firebase.auth.FacebookAuthProvider();
+      await this.afAuth.signInWithPopup(provider);
+      this.router.navigate(['/oxalate']);
+    } catch (error) {
+      console.error('Error during Facebook sign-in:', error);
+    }
+  }
+
+  isAuthenticated(): boolean {
+    return localStorage.getItem('user') !== null;
   }
 }
