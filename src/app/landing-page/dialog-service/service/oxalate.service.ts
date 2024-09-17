b0652bab3c7786c293c84e10540b25cba75fc3bb
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-09-17 13:37:13 -0400

    Retrieves all saved oxalates from Firestore

--- src/app/landing-page/dialog-service/service/oxalate.service.ts
+++ src/app/landing-page/dialog-service/service/oxalate.service.ts
@@ -1,13 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, Subject, of } from 'rxjs';
+import { Observable, Subject, from, of } from 'rxjs';
 import {
   map,
   debounceTime,
   distinctUntilChanged,
   switchMap,
+  catchError,
 } from 'rxjs/operators';
 import { Oxalate } from '../../model/oxalate';
+import { AuthService } from 'src/app/user-auth/service/auth-service.service';
+import {
+  AngularFirestore,
+  DocumentData,
+  DocumentReference,
+} from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root',
@@ -15,8 +22,13 @@ import { Oxalate } from '../../model/oxalate';
 export class OxalateService {
   private dataUrl = 'assets/mock-oxalate/oxolateListData.json';
   private searchTerms = new Subject<string>();
+  private collectionName = 'savedOxalates';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private firestore: AngularFirestore,
+    private authService: AuthService
+  ) {}
 
   getOxalateData(): Observable<Oxalate[]> {
     return this.http.get<Oxalate[]>(this.dataUrl);
@@ -65,10 +77,52 @@ export class OxalateService {
     this.searchTerms.next(query); // Emit the search term to trigger the dynamic search
   }
 
-  // OxalateService.ts
   getOxalateById(id: string): Observable<Oxalate | undefined> {
     return this.getOxalateData().pipe(
       map((oxalates) => oxalates.find((oxalate) => oxalate.id === id))
+    );
+  }
+
+  async saveOxalate(oxalateData: Oxalate): Promise<void> {
+    try {
+      const user = await this.authService.getCurrentUser();
+      if (!user) {
+        throw new Error('User not authenticated');
+      }
+
+      // Use `doc` to specify a document ID if necessary, or use auto-generated ID
+      const docRef = this.firestore
+        .collection(this.collectionName)
+        .doc(user.uid)
+        .collection('oxalates')
+        .doc(); // Creates a new document with auto-generated ID
+
+      await docRef.set({ ...oxalateData, userId: user.uid });
+      console.log('Oxalate saved successfully');
+    } catch (error) {
+      console.error('Error saving oxalate:', error);
+      throw error;
+    }
+  }
+
+  // Retrieves all saved oxalates from Firestore.
+  getSavedOxalates(): Observable<Oxalate[]> {
+    return from(this.authService.getCurrentUser()).pipe(
+      switchMap((user) => {
+        if (user) {
+          return this.firestore
+            .collection(this.collectionName)
+            .doc(user.uid)
+            .collection('oxalates')
+            .valueChanges() as Observable<Oxalate[]>;
+        } else {
+          return of([]); // Return an empty array if no user is authenticated
+        }
+      }),
+      catchError((error) => {
+        console.error('Error fetching saved oxalates:', error);
+        return of([]); // Return an empty array in case of error
+      })
     );
   }
 }
