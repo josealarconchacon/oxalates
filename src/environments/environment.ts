3a82a34f7cc41da4e5806188cd7bb864d5073cb3
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-10-07 12:22:50 -0400

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyCb9u5ZQDZ2HEQDGTfnzLBS8fTJhf7rUuQ',
-    authDomain: 'oxalates-c85a4.firebaseapp.com',
-    projectId: 'oxalates-c85a4',
-    storageBucket: 'oxalates-c85a4.appspot.com',
-    messagingSenderId: '1082420646964',
-    appId: '1:1082420646964:web:5da2e24b4c6fb6ce4e2661',
-    measurementId: 'G-5XZLJRGPWL',
-  },
+  firebaseConfig: {},
 };
