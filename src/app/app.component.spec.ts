7c6530ef70a4d980a7abd09df9f0a48371fac9c2
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-11-19 16:23:28 -0500

    still need to create a test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,15 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+class MockAppHeaderComponent {}
+
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -24,6 +28,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('oxalates app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'oxalates app is running!'
+    );
   });
 });
--- src/app/landing-page/footer/footer.component.spec.ts
+++ src/app/landing-page/footer/footer.component.spec.ts
@@ -1,21 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FooterComponent } from './footer.component';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon'; // Import MatIconModule
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'; // To handle animations
 
-describe('FooterComponent', () => {
+fdescribe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
+  let router: jasmine.SpyObj<Router>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [FooterComponent]
-    });
+  beforeEach(async () => {
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+    await TestBed.configureTestingModule({
+      declarations: [FooterComponent],
+      imports: [
+        MatIconModule, // Import MatIconModule
+        NoopAnimationsModule, // Optional: to avoid animation issues during testing
+      ],
+      providers: [{ provide: Router, useValue: routerSpy }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should call router.navigate with correct route when search is called', () => {
+    component.search();
+    expect(router.navigate).toHaveBeenCalledWith(['/oxalate']);
   });
 });
