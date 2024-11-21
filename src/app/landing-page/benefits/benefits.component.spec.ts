759d8dc7cad3d7384d56ef6ddc71f9f7270a5603
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-11-21 16:47:50 -0500

    test case

--- src/app/landing-page/benefits/benefits.component.spec.ts
+++ src/app/landing-page/benefits/benefits.component.spec.ts
@@ -1,15 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
 import { BenefitsComponent } from './benefits.component';
+import { BenefitsService } from './service/benefits.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
-describe('BenefitsComponent', () => {
+fdescribe('BenefitsComponent', () => {
   let component: BenefitsComponent;
   let fixture: ComponentFixture<BenefitsComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [BenefitsComponent],
+      providers: [
+        BenefitsService,
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
     });
+
     fixture = TestBed.createComponent(BenefitsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
