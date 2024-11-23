116dede0b6f226e8206ca2f1e4174e03023bc9b4
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-11-23 15:17:58 -0500

    need to complete test case for save component

--- src/app/user-auth/profile/save-items/save-items.component.spec.ts
+++ src/app/user-auth/profile/save-items/save-items.component.spec.ts
@@ -1,14 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SaveItemsComponent } from './save-items.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { of } from 'rxjs';
 
-describe('SaveItemsComponent', () => {
+// Mock db configuration
+const firebaseConfig = {
+  apiKey: 'mock-api-key',
+  authDomain: 'mock-auth-domain',
+  projectId: 'mock-project-id',
+  storageBucket: 'mock-storage-bucket',
+  messagingSenderId: 'mock-messaging-sender-id',
+  appId: 'mock-app-id',
+};
+
+// Mock AngularFirestore
+const angularFirestoreMock = {
+  collection: jasmine.createSpy('collection').and.returnValue({
+    valueChanges: jasmine.createSpy('valueChanges').and.returnValue(of([])),
+    doc: jasmine.createSpy('doc').and.returnValue({
+      set: jasmine.createSpy('set').and.returnValue(Promise.resolve()),
+    }),
+  }),
+};
+
+fdescribe('SaveItemsComponent', () => {
   let component: SaveItemsComponent;
   let fixture: ComponentFixture<SaveItemsComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [SaveItemsComponent]
+      declarations: [SaveItemsComponent],
+      imports: [
+        HttpClientTestingModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+      ],
+      providers: [
+        { provide: AngularFirestore, useValue: angularFirestoreMock },
+      ],
     });
     fixture = TestBed.createComponent(SaveItemsComponent);
     component = fixture.componentInstance;
