0a11f9d9bd4bac9329620a583a876b08d89a54c7
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-12-07 21:03:22 -0500

    add notification service

---
+++ src/app/landing-page/dialog-service/oxalate/service/notification.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  constructor(private snackBar: MatSnackBar) {}
+
+  show(message: string, action: string, panelClass: string[]): void {
+    this.snackBar.open(message, action, {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: panelClass,
+    });
+  }
+}
