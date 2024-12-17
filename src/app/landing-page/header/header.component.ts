f8e9e0030a300ec73d7a03b9b28640b0d2d12af0
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-12-16 20:55:34 -0500

    update nav

--- src/app/landing-page/header/header.component.ts
+++ src/app/landing-page/header/header.component.ts
@@ -1,3 +1,50 @@
+// import { Router } from '@angular/router';
+// import { Component, OnInit } from '@angular/core';
+// import { AuthService } from 'src/app/user-auth/service/auth-service.service';
+
+// @Component({
+//   selector: 'app-header',
+//   templateUrl: './header.component.html',
+//   styleUrls: ['./header.component.css'],
+// })
+// export class HeaderComponent implements OnInit {
+//   isNavOpen = false;
+//   isLoggedIn = false;
+//   userProfile: any;
+
+//   constructor(private router: Router, private authService: AuthService) {}
+
+//   ngOnInit(): void {
+//     this.authService.userProfile$.subscribe((user) => {
+//       console.log('User Profile:', user);
+//       this.isLoggedIn = !!user;
+//       this.userProfile = user;
+//     });
+//   }
+
+//   toggleNav() {
+//     this.isNavOpen = !this.isNavOpen;
+//   }
+//   closeNav(): void {
+//     this.isNavOpen = false;
+//   }
+
+//   goToLandingPage() {
+//     this.router.navigate(['/'], { queryParams: { scrollTo: 'top' } });
+//   }
+
+//   goToProfile() {
+//     if (this.isLoggedIn) {
+//       this.router.navigate(['/profile']);
+//     } else {
+//       this.router.navigate(['/auth']);
+//     }
+//   }
+
+//   search() {
+//     this.router.navigate(['/oxalate']);
+//   }
+// }
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/user-auth/service/auth-service.service';
