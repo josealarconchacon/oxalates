9ee3f62fabddbfee553ad7765ca30ce775f4b5ed
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-12-17 15:23:26 -0500

    fix navigation route

--- src/app/landing-page/footer/footer.component.ts
+++ src/app/landing-page/footer/footer.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+// import { Component } from '@angular/core';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-footer',
+//   templateUrl: './footer.component.html',
+//   styleUrls: ['./footer.component.css'],
+// })
+// export class FooterComponent {
+//   showBackToHome: boolean = true;
+//   constructor(private router: Router) {}
+//   search() {
+//     this.router.navigate(['/oxalate']);
+//   }
+// }
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {
-  constructor(private router: Router) {}
+export class FooterComponent implements OnInit {
+  showBackToHome: boolean = false;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // Check the current route and show/hide "Back to Home" based on the page
+    this.router.events.subscribe(() => {
+      const currentUrl = this.router.url;
+      if (currentUrl === '/oxalate') {
+        this.showBackToHome = true;
+      } else {
+        this.showBackToHome = false;
+      }
+    });
+  }
+
   search() {
     this.router.navigate(['/oxalate']);
   }
--- src/app/landing-page/header/header.component.ts
+++ src/app/landing-page/header/header.component.ts
@@ -1,51 +1,4 @@
-// import { Router } from '@angular/router';
-// import { Component, OnInit } from '@angular/core';
-// import { AuthService } from 'src/app/user-auth/service/auth-service.service';
-
-// @Component({
-//   selector: 'app-header',
-//   templateUrl: './header.component.html',
-//   styleUrls: ['./header.component.css'],
-// })
-// export class HeaderComponent implements OnInit {
-//   isNavOpen = false;
-//   isLoggedIn = false;
-//   userProfile: any;
-
-//   constructor(private router: Router, private authService: AuthService) {}
-
-//   ngOnInit(): void {
-//     this.authService.userProfile$.subscribe((user) => {
-//       console.log('User Profile:', user);
-//       this.isLoggedIn = !!user;
-//       this.userProfile = user;
-//     });
-//   }
-
-//   toggleNav() {
-//     this.isNavOpen = !this.isNavOpen;
-//   }
-//   closeNav(): void {
-//     this.isNavOpen = false;
-//   }
-
-//   goToLandingPage() {
-//     this.router.navigate(['/'], { queryParams: { scrollTo: 'top' } });
-//   }
-
-//   goToProfile() {
-//     if (this.isLoggedIn) {
-//       this.router.navigate(['/profile']);
-//     } else {
-//       this.router.navigate(['/auth']);
-//     }
-//   }
-
-//   search() {
-//     this.router.navigate(['/oxalate']);
-//   }
-// }
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/user-auth/service/auth-service.service';
 
@@ -58,8 +11,13 @@ export class HeaderComponent implements OnInit {
   isNavOpen = false;
   isLoggedIn = false;
   userProfile: any;
+  currentRoute: string = '';
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.authService.userProfile$.subscribe((user) => {
@@ -67,11 +25,17 @@ export class HeaderComponent implements OnInit {
       this.isLoggedIn = !!user;
       this.userProfile = user;
     });
+
+    // Get the current route
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.router.url;
+    });
   }
 
   toggleNav() {
     this.isNavOpen = !this.isNavOpen;
   }
+
   closeNav(): void {
     this.isNavOpen = false;
   }
