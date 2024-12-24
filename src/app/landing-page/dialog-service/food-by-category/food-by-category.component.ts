57aa71d43b30713b7596fdc2b53428b829ba3669
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-12-24 15:28:27 -0500

    refactor component

--- src/app/landing-page/dialog-service/food-by-category/food-by-category.component.ts
+++ src/app/landing-page/dialog-service/food-by-category/food-by-category.component.ts
@@ -1,21 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CategoryService } from '../oxalate/service/category.service';
 import { FilterService } from '../oxalate/service/filter.service';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-food-by-category',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './food-by-category.component.html',
   styleUrl: './food-by-category.component.css',
 })
-export class FoodByCategoryComponent {
+export class FoodByCategoryComponent implements OnInit {
+  cards: any[] = [];
+
   constructor(
     private router: Router,
+    private http: HttpClient,
     private categoryService: CategoryService,
-    private filterService: FilterService
+    private filterService: FilterService,
+    private sanitizer: DomSanitizer
   ) {}
+
+  ngOnInit(): void {
+    this.loadCards();
+  }
+
+  loadCards(): void {
+    this.http
+      .get<any[]>('../../../../assets/mock-oxalate/food-by-catejory.json')
+      .subscribe(
+        (data) => {
+          this.cards = data;
+          console.log('Cards loaded:', this.cards);
+        },
+        (error) => {
+          console.error('Error loading data:', error);
+        }
+      );
+  }
 
   onCardClick(category: string): void {
     console.log('Card clicked with category:', category);
@@ -25,5 +50,9 @@ export class FoodByCategoryComponent {
       calc_level: '',
     });
     this.router.navigate(['/oxalate']);
+  }
+
+  getSanitizedIcon(iconSvg: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(iconSvg);
   }
 }
