59bb85443bf1649638f7bdcacec77c94f6e88247
josealarconchacon <jose.alarcon.chacon@gmail.com>
2025-01-10 12:17:13 -0500

    update component

--- src/app/landing-page/contribution/contribution.component.ts
+++ src/app/landing-page/contribution/contribution.component.ts
@@ -1,45 +1,35 @@
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { ClipboardModule } from '@angular/cdk/clipboard';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contribution',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, ClipboardModule, FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contribution.component.html',
   styleUrls: ['./contribution.component.css'],
 })
-export class ContributionComponent implements OnInit {
-  goToLogin() {
-    throw new Error('Method not implemented.');
-  }
+export class ContributionComponent {
   currentStep = 1;
-  steps = [
-    { title: 'Contribution', description: 'Why Contributing' },
-    { title: 'How', description: 'How Can you Contribute' },
-  ];
+  verificationCode = '123456';
+  showModal = false;
+  selectedPaymentMethod: 'venmo' | 'paypal' | null = null;
 
-  ngOnInit(): void {}
-
-  continue(): void {
-    if (this.currentStep < this.steps.length) this.currentStep++;
-  }
-
-  previousStep(): void {
-    if (this.currentStep > 1) this.currentStep--;
-  }
-
-  setCurrentStep(step: number): void {
+  setStep(step: number): void {
     this.currentStep = step;
   }
 
-  isStepCompleted(index: number): boolean {
-    return index < this.currentStep - 1;
+  copyCode(): void {
+    navigator.clipboard.writeText(this.verificationCode);
+  }
+
+  openPaymentModal(method: 'venmo' | 'paypal'): void {
+    this.selectedPaymentMethod = method;
+    this.showModal = true;
+  }
+
+  closeModal(): void {
+    this.showModal = false;
+    this.selectedPaymentMethod = null;
   }
 }
