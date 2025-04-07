e4aa1e30359c1cc57f696b73ec49780ada62910c
josealarconchacon <jose.alarcon.chacon@gmail.com>
2025-04-07 18:38:34 -0400

    update search component

--- src/app/landing-page/dialog-service/oxalate/search-input/search-input.component.ts
+++ src/app/landing-page/dialog-service/oxalate/search-input/search-input.component.ts
@@ -5,6 +5,8 @@ import {
   Output,
   OnInit,
   OnDestroy,
+  ElementRef,
+  ViewChild,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -22,11 +24,15 @@ export class SearchInputComponent implements OnInit, OnDestroy {
   @Input() searchQuery: string = '';
   @Output() searchQueryChange = new EventEmitter<string>();
   @Output() clearSearch = new EventEmitter<void>();
+  @ViewChild('searchInputElement') searchInputElement?: ElementRef;
 
   isDarkTheme: boolean = false;
   private themeSubscription: Subscription | null = null;
 
-  constructor(private themeService: ThemeService) {}
+  constructor(
+    private themeService: ThemeService,
+    private elementRef: ElementRef
+  ) {}
 
   ngOnInit(): void {
     this.themeSubscription = this.themeService.isDarkTheme$.subscribe(
@@ -50,5 +56,21 @@ export class SearchInputComponent implements OnInit, OnDestroy {
     this.searchQuery = '';
     this.searchQueryChange.emit(this.searchQuery);
     this.clearSearch.emit();
+  }
+
+  /**
+   * Public method to focus the input element
+   */
+  focusInput(): void {
+    // First try to use ViewChild reference
+    if (this.searchInputElement) {
+      this.searchInputElement.nativeElement.focus();
+    } else {
+      // Otherwise look for input in the component
+      const inputElement = this.elementRef.nativeElement.querySelector('input');
+      if (inputElement) {
+        inputElement.focus();
+      }
+    }
   }
 }
